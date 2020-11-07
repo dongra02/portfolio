b3390b03577516b2ce6c25a6580cd24e1a1df313
dongra02 <dsgraham1@gmail.com>
2020-11-07 15:38:13 +0200

    project scroll functioning

--- scripts/app.js
+++ scripts/app.js
@@ -15,12 +15,34 @@ function init() {
   ]
 
   const skillsDiv = document.querySelector('.skills-list')
+  const prevBtn = document.querySelector('.prev')
+  const nextBtn = document.querySelector('.next')
+  const projOne = document.querySelector('.project-1')
+  const projTwo = document.querySelector('.project-2')
+  const projThree = document.querySelector('.project-3')
+  const projArray = [projOne, projTwo, projThree]
+
+  let currProjInd = 0
+
   icons.forEach(icon => {
     const iconElement = document.createElement('i')
     iconElement.classList.add(icon)
     skillsDiv.appendChild(iconElement)
   })
 
+  const scrollProject = (e) => {
+    projArray[currProjInd].style.display = 'none'
+    if (e.target.innerHTML === 'Next') {
+      currProjInd = currProjInd === 2 ? 0 : currProjInd + 1
+    } else {
+      currProjInd = currProjInd === 0 ? 2 : currProjInd - 1
+    }
+    projArray[currProjInd].style.display = 'flex'
+  }
+
+  prevBtn.addEventListener('click', scrollProject)
+  nextBtn.addEventListener('click', scrollProject)
+  
   // for header, use JS to add text after animation completes
 
 
